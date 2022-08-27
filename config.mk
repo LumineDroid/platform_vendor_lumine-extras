6c774af5df280393dd60891efb97d364343a0db4
Karan Parashar <whyredfire@gmail.com>
2022-08-27 13:54:24 +0530

    luminedroid: Import pixel_2016_exclusive XML for Google Photos
    Change-Id: I3cdb84f3e8d148b2619b5490bf18b744c5b417d4

--- config.mk
+++ config.mk
@@ -35,6 +35,10 @@ PRODUCT_COPY_FILES += \
     vendor/lumine-extras/fonts/GoogleSansFlex-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansFlex-Regular.ttf \
     vendor/lumine-extras/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
 
+# Google Photos Pixel Exclusive XML
+PRODUCT_COPY_FILES += \
+    vendor/lumine-extras/prebuilt/common/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
+
 # Overlay
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lumine-extras/overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/lumine-extras/overlay/common
