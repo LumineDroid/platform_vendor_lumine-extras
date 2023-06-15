a9515828c1bd5844f14499fad5f93806de5a78f6
aswin7469 <aswinas@pixysos.com>
2023-06-15 18:38:50 +0530

    luminedroid: fonts: Update from TQ3A.230605.012
    Change-Id: I4386110d5a29bf8d3625a60f004bc99f2f755047
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- config.mk
+++ config.mk
@@ -25,6 +25,7 @@ PRODUCT_COPY_FILES += \
     vendor/lumine-extras/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
     vendor/lumine-extras/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
     vendor/lumine-extras/fonts/GoogleSansClock-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansClock-Regular.ttf \
+    vendor/lumine-extras/fonts/GoogleSansFlex-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansFlex-Regular.ttf \
     vendor/lumine-extras/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
 
 # Overlay
